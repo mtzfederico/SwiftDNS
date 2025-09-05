afdd72cbaab36ed3d0ac66aa8425217fc0d726a7
Federico Martinez <fede@mtzfederico.com>
2025-09-05 14:29:42 -0400

    dropped ios 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftDNS",
     platforms: [
-        .macOS(.v10_14), .iOS(.v12), .tvOS(.v12)
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
